fac6828418c870441c38c66c31c48db207b294e5
lucbazzani <lu.bazzani@hotmail.com>
2021-10-16 19:08:56 -0300

    Program2 and Department findById

---
+++ src/application/Program2.java
@@ -0,0 +1,25 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+
+        System.out.println("=== TEST 1: department findById ===");
+        Department department = departmentDao.findById(3);
+        System.out.println(department);
+
+
+
+        scanner.close();
+    }
+}
