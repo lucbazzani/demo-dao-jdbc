8a3896e2be35cfddfc1f72fae3ed82e276cb0d6a
lucbazzani <lu.bazzani@hotmail.com>
2021-10-17 10:18:03 -0300

    Department Update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -30,6 +30,12 @@ public class Program2 {
         departmentDao.insert(newDepartment);
         System.out.println("Inserted! New id = " + newDepartment.getId());
 
+        System.out.println("\n=== TEST 4: department update ===");
+        department = departmentDao.findById(6);
+        department.setName("Cinema");
+        departmentDao.update(department);
+        System.out.println("Update complete!");
+
 
         scanner.close();
     }
