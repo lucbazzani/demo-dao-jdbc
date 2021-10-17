ab6b19de09e293639596a918af22f7253de33a42
lucbazzani <lu.bazzani@hotmail.com>
2021-10-17 10:09:59 -0300

    Department insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -25,6 +25,10 @@ public class Program2 {
             System.out.println(dep);
         }
 
+        System.out.println("\n=== TEST 3: department insert ===");
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id = " + newDepartment.getId());
 
 
         scanner.close();
